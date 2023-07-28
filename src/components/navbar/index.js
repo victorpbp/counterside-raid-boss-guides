70911766bb6f216388d02df874bda66a0912b05b
Musicii-bit <watchujae@gmail.com>
2023-07-27 19:29:50 -0700

    I hope I am doing this right and I hope that nothing breaks....

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,7 +2,31 @@ import './index.css';
 
 function Navbar() {
   return (
-    <div>This is the Navbar/Header</div>
+    <div class="navigation-bar">
+      <a href="#boss-description">
+        <h2>Boss Description</h2>
+      </a>
+
+      <a href="#recommended-ships">
+        <h2>Recommended Ships</h2>
+      </a>
+
+      <a href="#recommended-operators">
+        <h2>Recomemended operators</h2>
+      </a>
+
+      <a href="#recommended-dps">
+        <h2>Recommended DPS</h2>
+      </a>
+
+      <a href="#recommended-support">
+        <h2>Recommended Support</h2>
+      </a>
+
+      <a href="#extra-info">
+        <h2>Extra Info about the Boss</h2>
+      </a>
+    </div>
   );
 }
 
