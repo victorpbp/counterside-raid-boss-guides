378672203d4cfefd763cbb8144a24f151dfc73b1
victorpbp <victorrpeixotoo@gmail.com>
2023-08-01 18:46:10 -0300

    Adding ideas for the Extra Boss Info section

--- src/components/extraBossInfo/index.js
+++ src/components/extraBossInfo/index.js
@@ -2,6 +2,11 @@ import './index.css';
 import { useBosses } from '../../context/BossesContext';
 
 
+//We should absolutely make a proper section explaining not only how to play the boss
+//but also how many of each role and when to deploy
+//Like how many defenders, when and why... that kind of stuff
+//Make this section more like a "How to play" against the boss.
+
 function ExtraBossInfo() {
 
   const content = useBosses();
