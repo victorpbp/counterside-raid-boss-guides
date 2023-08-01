cd6634d5e9f1f1cc399a6bcc639a81e608dec24d
victorpbp <victorrpeixotoo@gmail.com>
2023-08-01 18:24:56 -0300

    Adding Damage Dealers Section and structure

--- src/components/dpsSection/index.js
+++ src/components/dpsSection/index.js
@@ -1,12 +1,45 @@
 import './index.css';
 import { useDPS } from '../../context/DpsContext';
+import { useGlobalSelector } from '../../context/RaidGuideContext';
+
+import StandardCard from '../standardcard';
+import StandoutCard from '../standoutcard';
 
 function DpsSection() {
 
-  const content = useDPS();
 
+  const content = useDPS();
+  const [globalSelector] = useGlobalSelector();
+  console.log(content);
+  
   return (
-    <div>This is the Best DPS's Section {content[0][0].name}</div>
+    <div className='sectionDPS'>
+
+      <h1>Damage Dealers</h1>
+
+      <div className='allcardsDPS'>
+
+        <div className='carddisplayDPS standoutDPS'>
+          {content[globalSelector].map((item, index) =>
+            { if (index<4) return (
+                <StandoutCard name={item.name} tier={item.tier} image={item.image} description={item.description}/>
+              ); else return ("")
+            }
+          )}
+        </div>
+
+        <div className='carddisplayDPS standardDPS'>
+          {content[globalSelector].map((item, index) =>
+            { if (index>=4) return (
+                <StandardCard name={item.name} tier={item.tier} image={item.image} description={item.description}/>
+              ); else return ("")
+            }
+          )}
+        </div>
+
+      </div>
+
+    </div>
   );
 }
 
