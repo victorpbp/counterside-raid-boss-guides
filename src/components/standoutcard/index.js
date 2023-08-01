c9c9270aa05c702105b6f359edfbf605eafdb91f
victorpbp <victorrpeixotoo@gmail.com>
2023-08-01 18:37:39 -0300

    Fixing styling issues and changing colors to better match roles

--- src/components/standoutcard/index.js
+++ src/components/standoutcard/index.js
@@ -5,7 +5,7 @@ function StandoutCard(props) {
   return (
     <div className='individualcard crucial'>
       <h3>{props.name} - {props.tier}</h3>
-      <img src={props.image} alt="" width="180px" height="180px"/>
+      <img src={props.image} alt="" width="160px" height="160px"/>
       <p>{props.description}</p>
     </div>
   );
--- src/components/supportsSection/index.js
+++ src/components/supportsSection/index.js
@@ -1,12 +1,44 @@
 import './index.css';
 import { useSupports } from '../../context/SupportsContext';
+import { useGlobalSelector } from '../../context/RaidGuideContext';
+
+import StandardCard from '../standardcard';
+import StandoutCard from '../standoutcard';
 
 function SupportsSection() {
 
   const content = useSupports();
-
+  const [globalSelector] = useGlobalSelector();
+  console.log(content);
+  
   return (
-    <div>This is the Best Supports Section {content[0][0].name}</div>
+    <div className='sectionSupports'>
+
+      <h1>Supports</h1>
+
+      <div className='allcardsSupports'>
+
+        <div className='carddisplaySupports standoutSupports'>
+          {content[globalSelector].map((item, index) =>
+            { if (index<4) return (
+                <StandoutCard name={item.name} tier={item.tier} image={item.image} description={item.description}/>
+              ); else return ("")
+            }
+          )}
+        </div>
+
+        <div className='carddisplaySupports standardSupports'>
+          {content[globalSelector].map((item, index) =>
+            { if (index>=4) return (
+                <StandardCard name={item.name} tier={item.tier} image={item.image} description={item.description}/>
+              ); else return ("")
+            }
+          )}
+        </div>
+
+      </div>
+
+    </div>
   );
 }
 
