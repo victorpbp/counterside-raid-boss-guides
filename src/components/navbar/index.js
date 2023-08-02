0bd26528e704282ecce18f02572a2f463f592249
Musicii-bit <watchujae@gmail.com>
2023-08-01 17:33:09 -0700

    Added media queries to the navigation bar and boss section as well as boss avatars in the navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -3,29 +3,38 @@ import './index.css';
 function Navbar() {
   return (
     <div class="navigation-bar">
-      <a href="#boss-description">
-        <h2>Boss Description</h2>
-      </a>
 
-      <a href="#recommended-ships">
-        <h2>Recommended Ships</h2>
-      </a>
+      <div class="navbar-images">
+        <img title="Britra" src='/assets/Bosspictures/Britra-avatar.png' alt= "Britra Avatar"></img>
+        <img title="Inhibitor" src='/assets/Bosspictures/Inhibitor-avatar.png' alt= "Inhibitor Avatar"></img>
+        <img title="Worm" src='/assets/Bosspictures/Worm-avatar.png' alt= "Worm Avatar"></img>
+      </div>
 
-      <a href="#recommended-operators">
-        <h2>Recomemended operators</h2>
-      </a>
+      <div class="navigation-bar-links">
+        <a href="#boss-description">
+          <h2>Boss Details</h2>
+        </a>
 
-      <a href="#recommended-dps">
-        <h2>Recommended DPS</h2>
-      </a>
+        <a href="#recommended-ships">
+          <h2>Ships</h2>
+        </a>
 
-      <a href="#recommended-support">
-        <h2>Recommended Support</h2>
-      </a>
+        <a href="#recommended-operators">
+          <h2>Operators</h2>
+        </a>
 
-      <a href="#extra-info">
-        <h2>Extra Info about the Boss</h2>
-      </a>
+        <a href="#recommended-dps">
+          <h2>DPS</h2>
+        </a>
+
+        <a href="#recommended-support">
+          <h2>Support</h2>
+        </a>
+
+        <a href="#extra-info">
+          <h2>Extra</h2>
+        </a>
+      </div>
     </div>
   );
 }
